27b86ee3630f1763406163e5c5485268459419d8
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-12-13 20:18:17 -0500

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -4,12 +4,5 @@ Date: CURDATE()
 Task: script that ranks country origins of bands,
 ordered by the number of (non-unique) fans
 */
-SELECT
-    origin,
-    COUNT(DISTINCT fan_id) AS nb_fans
-FROM
-    metal_bands
-GROUP BY
-    origin
-ORDER BY
-    nb_fans DESC, origin;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
