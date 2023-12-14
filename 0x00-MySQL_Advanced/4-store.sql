1f5897d42859ed0cacfb063abbbaffe1360fe106
kwenziwa <kwenziwa@live.com>
2023-12-13 19:53:37 -0500

    local commit

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+/*
+Author: Kwenziwa Lizwi Khanyile
+Date: CURDATE()
+Tasks: Creates a trigger that decreases the 
+quantity of an item after adding a new order
+*/
+
+CREATE TRIGGER decrement
+AFTER INSERT
+ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
